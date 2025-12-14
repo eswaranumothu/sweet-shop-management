d5f4eca3ac1f7a7f53f1d6fdc212a5b6015afc44
Eswar_Anumothu <140474535+eswaranumothu@users.noreply.github.com>
2025-12-14 15:38:16 +0530

    Update CORS for new Vercel domain

--- backend/app/main.py
+++ backend/app/main.py
@@ -16,7 +16,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "https://sweet-shop-management.vercel.app",
-        "https://https://sweetshopbyeswar.vercel.app/",  # ✅ ADD THIS
+        "https://sweetshopbyeswar.vercel.app",  # ✅ ADD THIS
     ],
     allow_credentials=True,
     allow_methods=["*"],
